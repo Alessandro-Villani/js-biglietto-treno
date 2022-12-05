0d8bbee5040a0b57d1e7049e1de364574afe58fe
Alessandro-Villani <alessvill@libero.it>
2022-12-05 15:31:46 +0100

    edited outline and initialized variables

--- js/script.js
+++ js/script.js
@@ -22,10 +22,29 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 5. Ask user travel kilometers and store it in userKilometers variable
 6. Ask user age and store it in userAge variable
 7. userKilometers and userAge data validation
-8. Calculate standard price and store in variable standardPrice
+8. Calculate standard price and store in variable price
 9. Verify if the user is underage or over 65
-    9a. if user underage apply underage discount and store in variable priceDiscounted
-    9b. if user over 65 apply over 65 discount and store in variable priceDiscounted
+    9a. if user underage apply underage discount and store in variable price
+    9b. if user over 65 apply over 65 discount and store in variable price
 10. Print final price on page
 ----------------------------------------------------------------------------------------*/
 
+//1. Initialize target
+
+const targetElement = document.getElementById('target');
+////console.log(targetElement);
+
+//2. Set price/km in a variable called kmPrice
+
+const kmPrice = 0.21;
+////console.log(kmPrice);
+
+// 3. Set a variable containing the value of underage discount called underageDiscount
+
+const underageDiscount = 0.2;
+////console.log(underageDiscount);
+
+// 4. Set a variable containing the value of over 65 discount called over65Discount
+
+const over65Discount = 0.4;
+////console.log(over65Discount);
